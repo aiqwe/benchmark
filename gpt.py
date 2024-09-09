c1d44b32d76b902d1cb9d051eb61c54e23b44efd
JayLee <sk8terbo2@gmail.com>
2024-09-09 21:33:45 +0900

    add gpt completion

--- gpt.py
+++ gpt.py
@@ -1,19 +1,28 @@
 import os
 from openai import OpenAI
 
-client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
-default_role = {
+DEFAULT_ROLE = {
     "translator": "I want you to ac as an English translator and NLP engineer. \
      Translate the text that I give you from English to Korean. \
-     Respond in Markdown"
+     Respond in Markdown. \
+     output format should be splitted by new line and mark '\n' delimeter each end of line."
 }
 
 
-def chat(text: str, model="gpt-4o-2024-08-06", role: str = "translator"):
+def chat(
+    text: str,
+    model="gpt-4o-2024-08-06",
+    role: str = DEFAULT_ROLE["translator"],
+    api_key: str = None,
+):
+    if not api_key:
+        api_key = os.environ["OPENAI_API_KEY"]
+
+    client = OpenAI(api_key=api_key)
     completion = client.chat.completions.create(
         model=model,
         messages=[
-            {"role": "system", "content": default_role[role]},
+            {"role": "system", "content": role},
             {"role": "user", "content": text},
         ],
         temperature=0,
